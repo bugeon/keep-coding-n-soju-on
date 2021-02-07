7265374f5d170f809e5e361ff36287094b28a33e
Bugeon <wittybear@naver.com>
2021-02-07 15:32:49 +0900

    update: Challenge03

--- Chapter08/Challenge03/josephus.js
+++ Chapter08/Challenge03/josephus.js
@@ -5,20 +5,32 @@ function queue_josephus(n, k) {
 		circle.push(i);
 	}
 	
+	let nth = 0;
 	while (circle.length !== 1) {
-		for (let i = 1; i <= k; i++) {
-			const eliminated = circle.shift()
+		const eliminated = circle.shift()
+		++nth;
 
-			if (i === k) {
-				// console.log(`${eliminated}`);
-				break;
-			}
-
-			circle.push(eliminated);
+		if (nth == k) {
+			nth = 0;
+			continue;
 		}
+		
+		circle.push(eliminated);
+
+		// for (let i = 1; i <= k; i++) {
+		// 	const eliminated = circle.shift()
+			
+
+		// 	if (i === k) {
+		// 		// console.log(`${eliminated}`);
+		// 		break;
+		// 	}
+
+		// 	circle.push(eliminated);
+		// }
 	}
 
-	console.log(`최종 생존자: ${circle.pop()}`);
+	console.log(`최종 생존자: ${circle.pop()}번`);
 }
 
 const n = 15;
